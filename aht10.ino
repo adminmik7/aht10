0b54b42ea790b66721d6c5923130b36bb1ac8b89
root <root@clawd.localdomain>
2026-04-07 06:45:21 +0000

    feat: add LCD 2004 support for main branch

--- aht10.ino
+++ aht10.ino
@@ -1,26 +1,41 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_AHTX0.h>
+#include <LiquidCrystal_I2C.h>
 
 Adafruit_AHTX0 aht;
+// Инициализация LCD 2004 (20 символов, 4 строки) по адресу 0x27
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
-  Serial.begin(9600); // Changed to 9600 for better stability (like nano1602)
-  delay(1000); // Wait for ESP to stabilize
+  Serial.begin(9600);
+  delay(1000);
   
-  Serial.println("AHT10 Sensor Start");
+  // Инициализация LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("AHT10 Monitor");
+  lcd.setCursor(0, 1);
+  lcd.print("Starting...");
 
   if (!aht.begin()) {
     Serial.println("Could not find AHT10. Check wiring!");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("AHT10 Error!");
     while (1) delay(10);
   }
   Serial.println("AHT10 found!");
+  delay(1000);
+  lcd.clear();
 }
 
 void loop() {
   sensors_event_t humidity, temp;
   aht.getEvent(&humidity, &temp);
 
+  // Вывод в Serial
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
   Serial.print(" *C\t");
@@ -28,6 +43,19 @@ void loop() {
   Serial.print("Humidity: ");
   Serial.print(humidity.relative_humidity);
   Serial.println(" %");
+
+  // Вывод на LCD 2004
+  lcd.setCursor(0, 1);
+  lcd.print("Temp: ");
+  lcd.print(temp.temperature, 1);
+  lcd.print(" C");
+  lcd.print("                "); // очистка остатков строки
+
+  lcd.setCursor(0, 2);
+  lcd.print("Humidity: ");
+  lcd.print(humidity.relative_humidity, 1);
+  lcd.print(" %");
+  lcd.print("               "); // очистка остатков строки
 
   delay(2000);
 }
